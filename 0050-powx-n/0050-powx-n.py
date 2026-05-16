3487869521c92b065e463d340a4e2cdd1413b5db
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-16 15:05:31 +0100

    Time: 0 ms (100%), Space: 19.3 MB (97.84%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        #Brute Force, TC - O(n), SC - O(1)
+        if n == 0 or x == 1.0:
+            return 1
+        if n < 0:
+            x = 1/x
+            n = -n
+        ans = 1
+        while n > 0:
+            if n % 2 == 1:
+                ans *= x
+            x *= x
+            n = n//2
+        return ans
