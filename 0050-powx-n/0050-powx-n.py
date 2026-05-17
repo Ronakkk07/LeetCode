c94c68150397fa82e5b579a296b9bd53ae788d66
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-17 10:35:09 +0100

    Time: 0 ms (100%), Space: 19.3 MB (97.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,25 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        #Brute Force, TC - O(n), SC - O(1)
-        if n == 0 or x == 1.0:
+        #Optimal Iterative Approach, TC - O(log(n)), SC - O(1) - Iterative soln
+        # if n == 0 or x == 1.0:
+        #     return 1
+        # if n < 0:
+        #     x = 1/x
+        #     n = -n
+        # ans = 1
+        # while n > 0:
+        #     if n % 2 == 1:
+        #         ans *= x
+        #     x *= x
+        #     n = n//2
+        # return ans
+
+        #Recursive Approach, TC - O(logN), SC - O(logN) (Recursive stack space is used)
+        if n == 0:
             return 1
         if n < 0:
             x = 1/x
             n = -n
-        ans = 1
-        while n > 0:
-            if n % 2 == 1:
-                ans *= x
-            x *= x
-            n = n//2
-        return ans
+        if n % 2 == 1:
+            return x * self.myPow(x, n-1)
+        return self.myPow(x*x, n//2)
