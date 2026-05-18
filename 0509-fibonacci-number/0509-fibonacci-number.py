d0f9a61ca9769285d4512a96aad2961539cd5395
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-18 12:00:48 +0100

    Time: 46 ms (65.47%), Space: 19.2 MB (57.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,22 @@
 class Solution:
     def fib(self, n: int) -> int:
         # TC - O(2**n), SC - O(n)
-        if n == 1:
-            return 1
-        if n == 0:
-            return 0
-        return self.fib(n-1) + self.fib(n-2) 
+        # if n == 1:
+        #     return 1
+        # if n == 0:
+        #     return 0
+        # if n <= 1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2) 
 
-        # O(n)
+        #  Dynamic Programming Tabulation TC - O(n), SC - O(n)
+        if n <= 1:
+            return n
+        f = [0] * (n+1)
+        f[0] = 0
+        f[1] = 1
+
+        for i in range(2, n+1):
+            f[i] = f[i-1] + f[i-2]
+        
+        return f[n]
