c883124902849fa85d11d2ce53410d21a6e7a43e
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-18 11:57:09 +0100

    Time: 331 ms (11.6%), Space: 19.2 MB (19.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
+        # TC - O(2**n), SC - O(n)
         if n == 1:
             return 1
         if n == 0:
             return 0
-        ans = self.fib(n-1) + self.fib(n-2)
-        return ans 
+        return self.fib(n-1) + self.fib(n-2) 
+
+        # O(n)
