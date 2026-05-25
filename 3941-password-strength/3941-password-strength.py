7531bb68a5b47f053b403eaeb3b9ad7dbc23ee48
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-25 11:41:53 +0100

    Time: 19 ms (100%), Space: 19.8 MB (100%) - LeetHub

--- 3941-password-strength/3941-password-strength.py
+++ 3941-password-strength/3941-password-strength.py
@@ -1,9 +1,16 @@
 class Solution:
     def passwordStrength(self, password: str) -> int:
+        # TC - O(N)
         digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        special_characters = ['!', '@', '#', '$']
         points = 0
-        unique = "".join(dict.fromkeys(password))
+        # unique = "".join(dict.fromkeys(password))
+        seen = set()
+        unique = []
+        for ch in password:
+            if ch not in seen:
+                seen.add(ch)
+                unique.append(ch)
+        unique = "".join(unique)
         for passwd in unique:
             if ord(passwd) >= 97 and ord(passwd) <= 122:
                 points += 1
