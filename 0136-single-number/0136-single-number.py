ab36314cc4a6a461e2a141bedf2924c5a36e96c3
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-28 09:47:47 +0100

    Time: 9 ms (18.04%), Space: 21.6 MB (19.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Brute Force - TC - O(N), SC - O(N)
+        count = {}
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        for key,value in count.items():
+            if value == 1:
+                return key
+        return -1
+
+        # XOR solution
+        # result = 0
+        # for num in nums:
+        #     result ^= num
+        # return result
