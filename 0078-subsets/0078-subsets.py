b90ccf33431fab0e0cdad0b86188037d79a26382
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-05-28 10:22:28 +0100

    Time: 0 ms (100%), Space: 19.3 MB (67.35%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,27 @@
 class Solution:
     # Recursion solution, TC - O(2**N), SC - O(N)
-    def function(self, index, nums, current, ans):
-        if index == len(nums):
-            ans.append(current[:])
-            return
-        current.append(nums[index])
-        self.function(index + 1, nums, current, ans)
-        current.pop()
-        self.function(index + 1, nums, current, ans)
+    # def function(self, index, nums, current, ans):
+    #     if index == len(nums):
+    #         ans.append(current[:])
+    #         return
+    #     current.append(nums[index])
+    #     self.function(index + 1, nums, current, ans)
+    #     current.pop()
+    #     self.function(index + 1, nums, current, ans)
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # ans = []
+        # self.function(0, nums, [], ans)
+        # return ans
+
+        # Bit Manipulation (PowerSet Technique)
+        n = len(nums)
         ans = []
-        self.function(0, nums, [], ans)
+        count = (1<<n)
+        for val in range(count):
+            subset = []
+            for i in range(n):
+                if val & (1 << i):
+                    subset.append(nums[i])
+            ans.append(subset)
         return ans
