2cfc972f265d213a48d09a1b4fe4cafb0a460135
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-06-04 12:26:44 +0100

    Time: 0 ms (100%), Space: 19.4 MB (38.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -23,17 +23,42 @@ class Solution:
         #     nums1[r] = arr[r]
 
         # Two pointers -> TC - O(min(N, M)) + O(NxlogN) + O(MxlogM) + O(N), SC - O(1)
-        left = m-1
-        right = 0
-        while left >= 0 and right < n:
-            if nums1[left] > nums2[right]:
-                nums1[left], nums2[right] = nums2[right], nums1[left]
-                left -= 1
+        # left = m-1
+        # right = 0
+        # while left >= 0 and right < n:
+        #     if nums1[left] > nums2[right]:
+        #         nums1[left], nums2[right] = nums2[right], nums1[left]
+        #         left -= 1
+        #         right += 1
+        #     else:
+        #         break
+        # nums1[:m] = sorted(nums1[:m])
+        # nums2.sort()
+        # for i in range(m, m + n):
+        #     nums1[i] = nums2[i - m]
+
+        # Gap Method -> O((N+M)xlog(N+M)), SC - O(1)
+        len = n+m
+        gap = (len//2) + (len % 2)
+
+        while gap > 0:
+            left = 0
+            right = left + gap
+            while right < len:
+                if left < m and right >= m:
+                    self.swapgreater(nums1, nums2, left, right - m)
+                elif left >= m:
+                    self.swapgreater(nums2, nums2, left-m, right - m)
+                else:
+                    self.swapgreater(nums1, nums1, left, right)
+                left += 1
                 right += 1
-            else:
+            if gap == 1:
                 break
-        nums1[:m] = sorted(nums1[:m])
-        nums2.sort()
-        for i in range(m, m + n):
-            nums1[i] = nums2[i - m]
+            gap = (gap//2) + (gap % 2)
+        for i in range(m, m+n):
+            nums1[i] = nums2[i-m]
+    def swapgreater(self, nums1, nums2, idx1, idx2):
+        if nums1[idx1] > nums2[idx2]:
+            nums1[idx1], nums2[idx2] = nums2[idx2], nums1[idx1]
 
