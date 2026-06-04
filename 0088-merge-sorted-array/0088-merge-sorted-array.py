23d8ebc27c311e36774b2dab466f143164f91eda
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-06-04 12:07:54 +0100

    Time: 0 ms (100%), Space: 19.1 MB (99.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,25 +1,39 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        arr = [0] * (m+n)
-        i = j = index = 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                arr[index] = nums1[i]
-                i += 1
+        # TC - O(N+M), SC - O(N+M)
+        # arr = [0] * (m+n)
+        # i = j = index = 0
+        # while i < m and j < n:
+        #     if nums1[i] <= nums2[j]:
+        #         arr[index] = nums1[i]
+        #         i += 1
+        #     else:
+        #         arr[index] = nums2[j]
+        #         j+=1
+        #     index += 1
+        # while i < m:
+        #     arr[index] = nums1[i]
+        #     i += 1
+        #     index+= 1
+        # while j < n:
+        #     arr[index] = nums2[j]
+        #     j += 1
+        #     index += 1
+        # for r in range(m+n):
+        #     nums1[r] = arr[r]
+
+        # Two pointers -> TC - O(min(N, M)) + O(NxlogN) + O(MxlogM) + O(N), SC - O(1)
+        left = m-1
+        right = 0
+        while left >= 0 and right < n:
+            if nums1[left] > nums2[right]:
+                nums1[left], nums2[right] = nums2[right], nums1[left]
+                left -= 1
+                right += 1
             else:
-                arr[index] = nums2[j]
-                j+=1
-            index += 1
-        while i < m:
-            arr[index] = nums1[i]
-            i += 1
-            index+= 1
-        while j < n:
-            arr[index] = nums2[j]
-            j += 1
-            index += 1
-        for r in range(m+n):
-            nums1[r] = arr[r]
+                break
+        nums1[:m] = sorted(nums1[:m])
+        nums2.sort()
+        for i in range(m, m + n):
+            nums1[i] = nums2[i - m]
+
