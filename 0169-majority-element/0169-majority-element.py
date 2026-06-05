9f6eb7e107a66e63d03fa79fbec7df9af2806139
Ronak Rajput <117246993+Ronakkk07@users.noreply.github.com>
2026-06-05 10:04:26 +0100

    Time: 3 ms (86.42%), Space: 21.2 MB (20.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,31 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # TC - O(n), SC - O(n)
+        # Moore's Voting Algorithm TC - O(n), SC - O(1)
         n = len(nums)
-        freq = {}
+        element = 0
+        count = 0
         for num in nums:
-            if num in freq:
-                freq[num] += 1
+            if count == 0:
+                count = 1
+                element = num
+            elif element == num:
+                count += 1
             else:
-                freq[num] = 1
-        for num, count in freq.items():
-            if count > n//2:
-                return num
+                count -= 1
+        cnt = nums.count(element)
+        if cnt > n//2:
+            return element
         return -1
+
+        # TC - O(n), SC - O(n)
+        # n = len(nums)
+        # freq = {}
+        # for num in nums:
+        #     if num in freq:
+        #         freq[num] += 1
+        #     else:
+        #         freq[num] = 1
+        # for num, count in freq.items():
+        #     if count > n//2:
+        #         return num
+        # return -1
